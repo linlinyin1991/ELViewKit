ccc907c9a4797b98f56b31affd139d3b518ce05d
yin linlin <linlin.yin@bkjk.com>
2018-02-07 14:21:07 +0800

    commit:ELViewKit Init

---
+++ ELViewKitDemo/ELViewKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ELViewKitDemo
+//
+//  Created by yin linlin on 2018/2/7.
+//  Copyright © 2018年 BKJK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ELViewKitDemo/ELViewKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ELViewKitDemo
+//
+//  Created by yin linlin on 2018/2/7.
+//  Copyright © 2018年 BKJK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
